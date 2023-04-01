738668df28038981bde2acfb7a21764416a74dcb
virendra <vkssingh0506@gmail.cim>
2023-04-02 00:43:40 +0530

    Added end points

---
+++ src/main/java/aks/app/aks/controller/AppController.java
@@ -0,0 +1,15 @@
+package aks.app.aks.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+public class AppController {
+
+    @GetMapping
+    public String getMessage(){
+        return "welcome";
+    }
+}
