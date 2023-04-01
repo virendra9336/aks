230ed90752ad566bf15b8967c2439ce2900ddaf6
virendra <vkssingh0506@gmail.cim>
2023-04-02 00:29:20 +0530

    first commit

---
+++ src/main/java/aks/app/aks/AksApplication.java
@@ -0,0 +1,13 @@
+package aks.app.aks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aks/app/aks/AksApplicationTests.java
@@ -0,0 +1,13 @@
+package aks.app.aks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
