064754de0f8757c9b2db4a6f9507684dd2a73010
virendra9336 <vspms1983@gmail.com>
2023-04-03 22:42:31 +0530

    Update AksApplicationTests.java

--- src/test/java/aks/app/aks/AksApplicationTests.java
+++ src/test/java/aks/app/aks/AksApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AksApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//@Test
+	//void contextLoads() {
+	//}
 
 }
